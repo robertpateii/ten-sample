99926867ff3a6bf281a0cc83c6e43d0e4175ff4c
Robert Pate <robert.pate@gmail.com>
2026-09-21 19:46:28 -0500

    Add rest of API calls

--- ApptApi/Program.cs
+++ ApptApi/Program.cs
@@ -6,7 +6,6 @@ builder.Services.AddDbContext<ApptDb>(opt => opt.UseInMemoryDatabase("ApptList")
 builder.Services.AddOpenApi();
 var app = builder.Build();
 
-// DateOnly.FromDateTime(DateTime.Now.AddDays(index))
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -24,14 +23,33 @@ app.MapGet("/appointments/{id}", async (int id, ApptDb db) =>
             ? Results.Ok(appt)
             : Results.NotFound());
 
+app.MapPost("/appointments", async (Appt appt, ApptDb db) =>
+{
+    db.Appts.Add(appt);
+    await db.SaveChangesAsync();
+});
 
+app.MapPut("/appointments/{id}", async (int id, Appt input, ApptDb db) =>
+{
+    var appt = await db.Appts.FindAsync(id);
+    if (appt is null) return Results.NotFound();
 
+    appt.PetName = input.PetName;
+    appt.Date = input.Date;
+    appt.OwnerName = input.OwnerName;
+    appt.OwnerAddress = input.OwnerAddress;
 
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 
+app.MapDelete("/appointments/{id}", async (int id, ApptDb db) =>
+{
+    var appt = await db.Appts.FindAsync(id);
+    if (appt is null) return Results.NotFound();
+    appt.IsArchived = true;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
